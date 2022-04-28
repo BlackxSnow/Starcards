b033f0b15ef89094204dd4ac9a416a3dd442f101
Black--Snow <calvinsoueid@gmail.com>
2022-04-28 12:47:35 +1000

    Refactor card dragging to Draggable

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -3,47 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Card : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
+[RequireComponent(typeof(Draggable))]
+public class Card : MonoBehaviour
 {
-    public static readonly Plane Board = new Plane(Vector3.back, 0);
+    private Draggable _Draggable;
 
-    private Vector2 _CursorOffset;
-    
-    private (Ray ray, float distance) RaycastBoard(PointerEventData data)
+    private bool _IsDraggable = true;
+
+    private void Awake()
     {
-        Ray ray = Camera.main.ScreenPointToRay(data.position);
-        bool isIntersecting = Board.Raycast(ray, out float distance);
+        _Draggable = GetComponent<Draggable>();
 
-        if (!isIntersecting)
-        {
-            throw new System.Exception("No interesection between board and cursor.");
-        }
-        return (ray, distance);
+        _Draggable.enabled = _IsDraggable;
     }
 
-    private Vector3 PosFromRayAndDistance(Ray ray, float distance)
+    public void SetDraggable(bool isDraggable)
     {
-        return Camera.main.transform.position + (ray.direction * distance);
-    }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        (Ray ray, float distance) = RaycastBoard(eventData);
-        Vector3 hitPos = PosFromRayAndDistance(ray, distance);
-        transform.position = new Vector3(hitPos.x - _CursorOffset.x, hitPos.y - _CursorOffset.y, transform.position.z);
-    }
-
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        (Ray ray, float distance) = RaycastBoard(eventData);
-        Vector3 hitPos = PosFromRayAndDistance(ray, distance);
-        _CursorOffset = new Vector2(hitPos.x - transform.position.x, hitPos.y - transform.position.y);
-
-        transform.Translate(0, 0, -0.5f);
-    }
-
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        transform.Translate(0, 0, 0.5f);
+        _IsDraggable = isDraggable;
+        _Draggable.enabled = _IsDraggable;
     }
 }
---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+    public static readonly Plane Board = new Plane(Vector3.back, 0);
+
+    public Action OnStartDrag;
+    public Action OnEndDrag;
+
+    private Vector2 _CursorOffset;
+
+    private (Ray ray, float distance) RaycastBoard(PointerEventData data)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(data.position);
+        bool isIntersecting = Board.Raycast(ray, out float distance);
+
+        if (!isIntersecting)
+        {
+            throw new System.Exception("No interesection between board and cursor.");
+        }
+        return (ray, distance);
+    }
+
+    private Vector3 PosFromRayAndDistance(Ray ray, float distance)
+    {
+        return Camera.main.transform.position + (ray.direction * distance);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        (Ray ray, float distance) = RaycastBoard(eventData);
+        Vector3 hitPos = PosFromRayAndDistance(ray, distance);
+        transform.position = new Vector3(hitPos.x - _CursorOffset.x, hitPos.y - _CursorOffset.y, transform.position.z);
+        OnStartDrag?.Invoke();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        (Ray ray, float distance) = RaycastBoard(eventData);
+        Vector3 hitPos = PosFromRayAndDistance(ray, distance);
+        _CursorOffset = new Vector2(hitPos.x - transform.position.x, hitPos.y - transform.position.y);
+
+        transform.Translate(0, 0, -0.5f);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        transform.Translate(0, 0, 0.5f);
+        OnEndDrag?.Invoke();
+    }
+}
